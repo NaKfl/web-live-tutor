1a372fb640ed1e6b950de7ac6d0d589f56b68c96
NaKfl <nguyenhuugiatri@gmail.com>
2021-05-12 20:00:20 +0700

    GEN-283 Re style profile page

--- src/app/containers/Profile/index.jsx
+++ src/app/containers/Profile/index.jsx
@@ -3,7 +3,7 @@ import Button from 'app/components/Button';
 import Form from 'app/components/Form';
 import Input from 'app/components/Input';
 import DatePicker from 'app/components/DatePicker';
-import { Avatar } from 'antd';
+import { Avatar, Collapse } from 'antd';
 import { Row, Col } from 'app/components/Grid';
 import useHooks from './hooks';
 import saga from './saga';
@@ -13,12 +13,13 @@ import {
   StyledAvatar,
   StyledProfile,
   StyledEditIcon,
+  StyledBasicInfo,
   StyledIconEdit,
+  StyledDetailInfo,
 } from './styles';
 import { useTranslation } from 'react-i18next';
-import { Typography } from 'antd';
 import UploadAvatar from './UploadAva';
-const { Title } = Typography;
+const { Panel } = Collapse;
 
 export const Profile = () => {
   useInjectSaga({ key: sliceKey, saga });
@@ -30,16 +31,9 @@ export const Profile = () => {
 
   return (
     <StyledProfile>
-      <Form
-        className="profile-form"
-        form={form}
-        onFinish={onFinish}
-        requiredMark={false}
-        initialValues={info ?? {}}
-        layout="vertical"
-      >
-        <Row className="mb-4 group-avatar">
-          <Col flex={0.05}>
+      <StyledBasicInfo>
+        <Col span={9}>
+          <Row justify="end">
             <StyledAvatar>
               <Avatar size={130} src={info?.avatar} />
               <StyledIconEdit onClick={openModal}>
@@ -51,74 +45,99 @@ export const Profile = () => {
                 {...modalControl}
               ></UploadAvatar>
             </StyledAvatar>
-          </Col>
-          <Col className="group-info">
-            <Title level={3}>{info?.name}</Title>
-            <Title level={5}>{info?.country}</Title>
-          </Col>
-        </Row>
+          </Row>
+        </Col>
+        <Col span={15} className="group-info">
+          <h3 level={3}>{info?.name || 'Anonymous'}</h3>
+          <span level={5}>{`${t('Profile.accountID')}: ${info?.id}`}</span>
+        </Col>
+      </StyledBasicInfo>
 
-        <Row gutter={[24, 0]}>
-          <Col span={12}>
-            <Form.Item
-              label={t('Profile.name')}
-              name="name"
-              rules={[
-                {
-                  required: true,
-                  message: t('Profile.requiredName'),
-                },
-              ]}
+      <StyledDetailInfo>
+        <Collapse className="collapse" defaultActiveKey={['1']}>
+          <Panel
+            className="align-center-panel"
+            showArrow={false}
+            header={t('Profile.account')}
+            key="1"
+          >
+            <Form
+              id="profile-form"
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 12 }}
+              layout="horizontal"
+              className="profile-form"
+              form={form}
+              onFinish={onFinish}
+              requiredMark={false}
+              initialValues={info ?? {}}
             >
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item
-              label={t('Profile.email')}
-              name="email"
-              rules={[
-                {
-                  type: 'email',
-                  message: t('Profile.invalidEmail'),
-                },
-                {
-                  required: true,
-                  message: t('Profile.requiredEmail'),
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label={t('Profile.phone')} name="phone">
-              <Input placeholder="Phone" />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label={t('Profile.country')} name="country">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label={t('Profile.language')} name="language">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label={t('Profile.birthday')} name="birthday">
-              <DatePicker />
-            </Form.Item>
-          </Col>
-        </Row>
-
-        <Form.Item>
-          <Button type="accent" htmlType="submit" loading={loading}>
-            {t('Profile.btnSave')}
-          </Button>
-        </Form.Item>
-      </Form>
+              <Form.Item
+                label={t('Profile.name')}
+                name="name"
+                rules={[
+                  {
+                    required: true,
+                    message: t('Profile.requiredName'),
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label={t('Profile.email')}
+                name="email"
+                rules={[
+                  {
+                    type: 'email',
+                    message: t('Profile.invalidEmail'),
+                  },
+                  {
+                    required: true,
+                    message: t('Profile.requiredEmail'),
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item label={t('Profile.phone')} name="phone">
+                <Input placeholder="Phone" />
+              </Form.Item>
+              <Form.Item label={t('Profile.country')} name="country">
+                <Input />
+              </Form.Item>
+              <Form.Item label={t('Profile.language')} name="language">
+                <Input />
+              </Form.Item>
+              <Col>
+                <Form.Item label={t('Profile.birthday')} name="birthday">
+                  <DatePicker />
+                </Form.Item>
+              </Col>
+            </Form>
+            <Row>
+              <Col span={4} offset={16}>
+                <Row justify="end">
+                  <Button
+                    form="profile-form"
+                    className="submit-btn"
+                    type="accent"
+                    htmlType="submit"
+                    loading={loading}
+                  >
+                    {t('Profile.btnSave')}
+                  </Button>
+                </Row>
+              </Col>
+            </Row>
+          </Panel>
+          <Panel
+            showArrow={false}
+            header={t('Profile.settings')}
+            key="2"
+          ></Panel>
+        </Collapse>
+      </StyledDetailInfo>
     </StyledProfile>
   );
 };
--- src/app/containers/Profile/styles/index.js
+++ src/app/containers/Profile/styles/index.js
@@ -1,34 +1,29 @@
 import EditIcon from 'app/components/EditIcon';
+import { Row } from 'app/components/Grid';
 import styled from 'styled-components';
+import { COLOR } from 'styles/colorPalette';
 import { media } from 'styles/media';
 
 export const StyledProfile = styled.div`
   display: flex;
-  justify-content: center;
-  .profile-form {
-    max-width: 800px;
-
-    .group-avatar {
-      ${media.mobile`
-          padding: 16px;
-          justify-content: center;
-      `}
-    }
-
-    .group-info {
-      justify-content: center;
-      display: flex;
-      flex-direction: column;
-      > h5 {
-        margin: 0;
-        opacity: 0.7;
-        font-weight: normal;
-      }
-    }
-  }
+  flex-direction: column;
+  width: 65%;
 `;
 
 export const StyledEditIcon = styled(EditIcon)`
+  display: inline-table;
+  position: absolute;
+  bottom: 5px;
+  right: 5px;
+  width: 28px;
+  height: 28px;
+  border-radius: 50%;
+  background-color: transparent;
+  box-shadow: 0px 4px 16px ${COLOR.SHADOW_GRAY};
+  cursor: pointer;
+`;
+
+export const StyledIconEdit = styled.div`
   display: inline-table;
   position: absolute;
   bottom: 0;
@@ -38,13 +33,10 @@ export const StyledEditIcon = styled(EditIcon)`
 export const StyledAvatar = styled.div`
   display: inline-flex;
   position: relative;
-`;
-
-export const StyledIconEdit = styled.div`
-  display: inline-table;
-  position: absolute;
-  bottom: 0;
-  right: 0;
+  margin-right: 20px;
+  border-radius: 50%;
+  box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%),
+    0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
 `;
 
 export const StyledDragImage = styled.div`
@@ -59,4 +51,69 @@ export const StyledDragImage = styled.div`
   border: 2px dashed #eee;
   border-radius: 2px;
   color: #bdbdbd;
+`;
+
+export const StyledBasicInfo = styled(Row)`
+  overflow: hidden;
+  padding: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 0 5px 0 rgb(0 0 0 / 16%);
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  border-top: solid 4px ${COLOR.CORNFLOWER};
+  .group-info {
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    h3 {
+      margin-bottom: 8px;
+      font-size: 24px;
+    }
+    span {
+      font-size: 14px;
+      color: rgba(0, 0, 0, 0.54);
+    }
+  }
+`;
+
+export const StyledDetailInfo = styled(Row)`
+  .collapse {
+    width: 100%;
+    box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%),
+      0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
+    border: none;
+    .ant-collapse-header {
+      background-color: ${COLOR.BROWN_F5};
+    }
+  }
+  .profile-form {
+    & > div {
+      width: 100%;
+    }
+    display: flex;
+    flex-direction: column;
+    .ant-form-item-label {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      font-weight: 600;
+      padding-right: 15px;
+      label:after {
+        content: none;
+      }
+    }
+
+    .submit-btn {
+      margin-left: auto;
+    }
+
+    .group-avatar {
+      ${media.mobile`
+          padding: 16px;
+          justify-content: center;
+      `}
+    }
+  }
 `;
