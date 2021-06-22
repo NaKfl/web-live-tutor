397a9e7caace37382c03dbdeb5818bc9aab0b27b
NaKfl <nguyenhuugiatri@gmail.com>
2021-06-23 04:52:57 +0700

    Try new version update .env

--- Dockerfile
+++ Dockerfile
@@ -15,4 +15,12 @@ EXPOSE 80
 COPY manifest/default.conf /etc/nginx/conf.d/
 COPY --from=builder /app/build /usr/share/nginx/html
 
-CMD ["/bin/bash", "-c", "nginx -g \"daemon off;\""]
+# Copy .env file and shell script to container
+WORKDIR /usr/share/nginx/html
+COPY ./env.sh .
+COPY .env .
+
+# Make our shell script executable
+RUN chmod +x env.sh
+
+CMD ["/bin/bash", "-c", "/usr/share/nginx/html/env.sh && nginx -g \"daemon off;\""]
--- src/configs/index.js
+++ src/configs/index.js
@@ -1,10 +1,14 @@
-export const WEB_API = process.env.REACT_APP_WEB_API;
-export const GOOGLE_ID = process.env.REACT_APP_GOOGLE_ID;
-export const FACEBOOK_ID = process.env.REACT_APP_FACEBOOK_ID;
-export const JWT_SECRET = process.env.REACT_APP_JWT_SECRET;
-export const vnp_TmnCode = process.env.REACT_APP_VNP_TMNCODE;
-export const vnp_HashSecret = process.env.REACT_APP_VNP_HASHSECRET;
-export const vnp_Url = process.env.REACT_APP_VNP_URL;
-export const vnp_ReturnUrl = process.env.REACT_APP_VNP_RETURNURL;
-export const REFRESH_TOKEN_INTERVAL_MINUTES =
-  process.env.REACT_APP_REFRESH_TOKEN_INTERVAL_MINUTES;
+export const configs = {
+  WEB_API: window._env_.REACT_APP_WEB_API,
+  GOOGLE_ID: window._env_.REACT_APP_GOOGLE_ID,
+  FACEBOOK_ID: window._env_.REACT_APP_FACEBOOK_ID,
+  JWT_SECRET: window._env_.REACT_APP_JWT_SECRET,
+  vnp_TmnCode: window._env_.REACT_APP_VNP_TMNCODE,
+  vnp_HashSecret: window._env_.REACT_APP_VNP_HASHSECRET,
+  vnp_Url: window._env_.REACT_APP_VNP_URL,
+  vnp_ReturnUrl: window._env_.REACT_APP_VNP_RETURNURL,
+  REFRESH_TOKEN_INTERVAL_MINUTES:
+    window._env_.REACT_APP_REFRESH_TOKEN_INTERVAL_MINUTES,
+};
+
+export default configs;
