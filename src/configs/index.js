1fa83efe522fb9b07393821b6460fac5f3b61900
NaKfl <nguyenhuugiatri@gmail.com>
2021-06-23 05:02:34 +0700

    Change configs file

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,14 +1,10 @@
-export const configs = {
-  WEB_API: window._env_.REACT_APP_WEB_API,
-  GOOGLE_ID: window._env_.REACT_APP_GOOGLE_ID,
-  FACEBOOK_ID: window._env_.REACT_APP_FACEBOOK_ID,
-  JWT_SECRET: window._env_.REACT_APP_JWT_SECRET,
-  vnp_TmnCode: window._env_.REACT_APP_VNP_TMNCODE,
-  vnp_HashSecret: window._env_.REACT_APP_VNP_HASHSECRET,
-  vnp_Url: window._env_.REACT_APP_VNP_URL,
-  vnp_ReturnUrl: window._env_.REACT_APP_VNP_RETURNURL,
-  REFRESH_TOKEN_INTERVAL_MINUTES:
-    window._env_.REACT_APP_REFRESH_TOKEN_INTERVAL_MINUTES,
-};
-
-export default configs;
+export const WEB_API = window._env_.REACT_APP_WEB_API;
+export const GOOGLE_ID = window._env_.REACT_APP_GOOGLE_ID;
+export const FACEBOOK_ID = window._env_.REACT_APP_FACEBOOK_ID;
+export const JWT_SECRET = window._env_.REACT_APP_JWT_SECRET;
+export const vnp_TmnCode = window._env_.REACT_APP_VNP_TMNCODE;
+export const vnp_HashSecret = window._env_.REACT_APP_VNP_HASHSECRET;
+export const vnp_Url = window._env_.REACT_APP_VNP_URL;
+export const vnp_ReturnUrl = window._env_.REACT_APP_VNP_RETURNURL;
+export const REFRESH_TOKEN_INTERVAL_MINUTES =
+  window._env_.REACT_APP_REFRESH_TOKEN_INTERVAL_MINUTES;
