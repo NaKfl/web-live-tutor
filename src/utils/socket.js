16285cbba970ee347d147d834e53efc3972418ee
NaKfl <nguyenhuugiatri@gmail.com>
2021-06-23 07:44:58 +0700

    Add credential for socket io

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,8 @@
 import { WEB_API } from 'configs';
 import io from 'socket.io-client';
 
-const socket = io(WEB_API);
+const socket = io(WEB_API, {
+  withCredentials: true,
+});
 
 export default socket;
