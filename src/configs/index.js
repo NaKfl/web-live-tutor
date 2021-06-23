8919b210d0b541de9cc2bae3fadd0259c24cb018
Nguyen Huu Gia Tri <44458003+NaKfl@users.noreply.github.com>
2021-06-23 17:22:28 +0700

    Deploy (#92)
    * Fix import (#91)
    
    * Try new version
    
    * Try new version update .env
    
    * Change configs file
    
    * add .env
    
    * Add credential for socket io
    
    * Fix cluster mode issues
    
    * enable websocket nginx config
    
    * Remove http video link

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,10 +1,10 @@
-export const WEB_API = process.env.REACT_APP_WEB_API;
-export const GOOGLE_ID = process.env.REACT_APP_GOOGLE_ID;
-export const FACEBOOK_ID = process.env.REACT_APP_FACEBOOK_ID;
-export const JWT_SECRET = process.env.REACT_APP_JWT_SECRET;
-export const vnp_TmnCode = process.env.REACT_APP_VNP_TMNCODE;
-export const vnp_HashSecret = process.env.REACT_APP_VNP_HASHSECRET;
-export const vnp_Url = process.env.REACT_APP_VNP_URL;
-export const vnp_ReturnUrl = process.env.REACT_APP_VNP_RETURNURL;
+export const WEB_API = window._env_.REACT_APP_WEB_API;
+export const GOOGLE_ID = window._env_.REACT_APP_GOOGLE_ID;
+export const FACEBOOK_ID = window._env_.REACT_APP_FACEBOOK_ID;
+export const JWT_SECRET = window._env_.REACT_APP_JWT_SECRET;
+export const vnp_TmnCode = window._env_.REACT_APP_VNP_TMNCODE;
+export const vnp_HashSecret = window._env_.REACT_APP_VNP_HASHSECRET;
+export const vnp_Url = window._env_.REACT_APP_VNP_URL;
+export const vnp_ReturnUrl = window._env_.REACT_APP_VNP_RETURNURL;
 export const REFRESH_TOKEN_INTERVAL_MINUTES =
-  process.env.REACT_APP_REFRESH_TOKEN_INTERVAL_MINUTES;
+  window._env_.REACT_APP_REFRESH_TOKEN_INTERVAL_MINUTES;
