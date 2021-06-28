f10d304e84c60c0aaf11ff2feb863849f06de17a
NaKfl <nguyenhuugiatri@gmail.com>
2021-06-28 13:55:01 +0700

    Detect mobile and enhance jitsi flows

--- src/app/containers/JitsiMeetPage/index.jsx
+++ src/app/containers/JitsiMeetPage/index.jsx
@@ -1,23 +1,63 @@
+import { Spin, Button as ButtonAntd } from 'antd';
 import { JitsiMeet } from 'app/components/JitsiMeet';
 import React, { memo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useInjectReducer, useInjectSaga } from 'utils/reduxInjectors';
 import useHooks from './hooks';
 import saga from './saga';
 import { reducer, sliceKey } from './slice';
 import { StyledMeetingPage } from './styles';
-import { Spin } from 'antd';
-import { useTranslation } from 'react-i18next';
+import Button from 'app/components/Button';
 
 export const JitsiMeetPage = props => {
   useInjectSaga({ key: sliceKey, saga });
   useInjectReducer({ key: sliceKey, reducer });
   const { handlers, selectors } = useHooks(props);
-  const { roomInfo, isLoading } = selectors;
-  const { handleSomeOneLeave, endCall } = handlers;
+  const {
+    roomInfo,
+    isLoading,
+    isOnMobile,
+    error,
+    token,
+    isContinueWeb,
+  } = selectors;
+  const { handleSomeOneLeave, endCall, continueGoToWeb } = handlers;
   const { t } = useTranslation();
 
+  if (error)
+    return (
+      <Spin
+        spinning
+        tip={
+          <>
+            <p style={{ marginTop: 20 }}>{error}</p>
+            <p>{t('Jitsi.backToHome')}</p>
+          </>
+        }
+      >
+        <StyledMeetingPage />
+      </Spin>
+    );
+
+  if (isOnMobile && !isContinueWeb)
+    return (
+      <StyledMeetingPage>
+        <a href={`livetutoring://token=${token}`}>
+          <Button type="accent">{t('Jitsi.gotToApp')}</Button>
+        </a>
+        <div>
+          <ButtonAntd type="link" onClick={continueGoToWeb}>
+            {t('Jitsi.continueGoToWeb')}
+          </ButtonAntd>
+        </div>
+      </StyledMeetingPage>
+    );
+
   return (
-    <Spin spinning={isLoading} tip={`${t('Common.backToHome')} ...`}>
+    <Spin
+      spinning={isLoading}
+      tip={<p style={{ marginTop: 20 }}>{t('Jitsi.backToHome')}</p>}
+    >
       <StyledMeetingPage isTutor={roomInfo.isTutor}>
         {roomInfo.roomName && (
           <JitsiMeet
@@ -26,7 +66,7 @@ export const JitsiMeetPage = props => {
             disableInviteFunctions={true}
             onMeetingEnd={() => endCall()}
             onSomeOneLeave={() => handleSomeOneLeave(roomInfo)}
-            loadingComponent={<p>ʕ •ᴥ•ʔ jitsi is loading ...</p>}
+            loadingComponent={<p>{t('Common.loading')}</p>}
           />
         )}
       </StyledMeetingPage>
--- src/app/containers/JitsiMeetPage/styles/index.js
+++ src/app/containers/JitsiMeetPage/styles/index.js
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
 
 export const StyledMeetingPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100vh;
   .jitsi-container {
-      .button-group-right {
-        visibility: ${({ isTutor }) => (isTutor ? 'hidden' : 'hidden')};
-      }
+    .button-group-right {
+      visibility: ${({ isTutor }) => (isTutor ? 'hidden' : 'hidden')};
     }
+  }
+  .content {
+    margin: auto 0;
   }
 `;
