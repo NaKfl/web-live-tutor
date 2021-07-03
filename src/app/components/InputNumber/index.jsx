f812e262b5c4bae9caeb16ecbeed590397e65faa
HoaiBao <55429274+hbsolider@users.noreply.github.com>
2021-07-03 23:30:16 +0700

    update fee tutor (#133)

--- src/app/components/InputNumber/index.jsx
+++ src/app/components/InputNumber/index.jsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 import { StyledInputNumber } from './styles';
 
 const InputNumber = ({ ...rest }) => {
-  return <StyledInputNumber {...rest} />;
+  return <StyledInputNumber defaultValue={0} {...rest} />;
 };
 
 export default memo(InputNumber);
